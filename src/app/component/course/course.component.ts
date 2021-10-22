bb84700cec4b4e80c069cccf00921354805e3fe4
YBPrasad <ybuddhikaprasad@gmail.com>
2021-10-22 20:55:13 +0530

    course table value

--- src/app/component/course/course.component.ts
+++ src/app/component/course/course.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/course';
+import { CourseService } from 'src/app/service/course.service';
 
 @Component({
   selector: 'app-course',
@@ -6,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  course?:Course[]
 
-  constructor() { }
+  displayedColumns: string[] = ['Id', 'Name', '#'];
+  dataSource:Course[]=[]
+
+  constructor(private courseSer:CourseService) { }
 
   ngOnInit(): void {
+    this.reloadData()
+  }
 
+  
+
+  reloadData(){
+    this.courseSer.getAllCourses().subscribe(data=>{
+      this.dataSource=data
+    },error=>{
+      console.log(error)
+    })
   }
 
   
